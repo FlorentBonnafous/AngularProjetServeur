497fb98831e176dfc8ab15bb1f71148e8ae88a1b
FlorentBonnafous <florentbonnafousts1@gmail.com>
2020-03-24 19:01:30 +0100

    Retour coté serveur : -récup username -achat produits (bugs) -achat managers (bugs)

--- com.isis.adventureISIServer/com.isis.adventureISIServer/src/main/java/com/example/com/isis/adventureISIServer/Webservice.java
+++ com.isis.adventureISIServer/com.isis.adventureISIServer/src/main/java/com/example/com/isis/adventureISIServer/Webservice.java
@@ -5,30 +5,54 @@
  */
 package com.example.com.isis.adventureISIServer;
 
+import generated.PallierType;
+import generated.ProductType;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
 import static javax.swing.text.html.FormSubmitEvent.MethodType.GET;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBException;
 
 /**
  *
  * @author Flore
  */
 @Path("generic")
-public class Webservice { 
-    
+public class Webservice {
+
     Services services;
-    
+
     public Webservice() {
         services = new Services();
     }
-    
+
     @GET
     @Path("world")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response getWorld(){
-        return Response.ok(services.getWorld()).build();
+    public Response getXml(@Context HttpServletRequest request) throws JAXBException, IOException {
+        String username = request.getHeader("X-user");
+        //System.out.println(username+"webservice");
+        return Response.ok(services.getWorld(username)).build();
     }
+
+    @PUT
+    @Path("product")
+    public void putProduct(@Context HttpServletRequest request, ProductType product) throws JAXBException, IOException {
+        String username = request.getHeader("X-user");
+        services.updateProduct(username, product);
+    }
+
+    @PUT
+    @Path("manager")
+    public void putManager(@Context HttpServletRequest request, PallierType manager) throws JAXBException, IOException {
+        String username = request.getHeader("X-user");
+        services.updateManager(username, manager);
+    }
+
 }
